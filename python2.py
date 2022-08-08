dc526c24614a034e1312de3c8c8703a5fc1504d8
jono2121 <110508880+jono2121@users.noreply.github.com>
2022-08-07 18:51:58 -0700

    Add files via upload

---
+++ python2.py
@@ -0,0 +1,11 @@
+# belajar output
+
+nama = "gerald" # gerald adalah data, nama adalah variable
+umur = 15
+alamat = "margonda"
+
+print(nama)
+print(umur)
+print(alamat)
+
+print("nama saya adalah " + nama)
